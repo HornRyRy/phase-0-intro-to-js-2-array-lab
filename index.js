1d62c12be32e7adef1d6afa0857bd0e698bdcdef
Ryan Horn <rj.horn@gmail.com>
2022-11-14 08:55:17 -0700

    commit tree 7a8a993207eb0c419835a92a688178689d134df4

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
 // Write your solution here!
+// copied code clip from testing page
+// describe('index.js', function () {
+//     describe('cats', function () {
+//       it('is assigned an initial value of ["Milo", "Otis", "Garfield"]', function () {
+//         expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
+//       });
+//     });
+  
+//     describe('Array functions', function () {
+//       beforeEach(function () {
+//         cats.length = 0;
+  
+//         cats.push('Milo', 'Otis', 'Garfield');
+//       });
+
+
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+}
+
+function appendCat(name){
+    
+    const newCats = [...cats];
+    newCats.push(name);
+    return newCats;
+}
+
+function prependCat(name){
+    
+    const newCats = [...cats];
+    newCats.unshift(name);
+    return newCats;
+}
+
+function removeLastCat(name){
+    
+    const newCats = [...cats];
+    newCats.pop(name);
+    return newCats;
+}
+
+function removeFirstCat(name){
+    
+    const newCats = [...cats];
+    newCats.shift(name);
+    return newCats;
+}
